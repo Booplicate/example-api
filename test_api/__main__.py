d7170a2c5f85e9e2710d541cdc70662fd593beec
Totally a booplicate <53382877+Booplicate@users.noreply.github.com>
2022-10-24 19:59:05 +0300

    allow specify host and port via env vars

--- test_api/__main__.py
+++ test_api/__main__.py
@@ -3,6 +3,7 @@ Program entry point
 """
 
 import argparse
+import os
 
 import uvicorn
 
@@ -14,20 +15,33 @@ APP_NAME = "test_api.application:main_app"
 
 def _parse_args():
     parser = argparse.ArgumentParser(prog="test_api", description="An example API")
-    parser.add_argument("host", help="server address")
-    parser.add_argument("port", type=int, help="server address")
+    parser.add_argument("--host", default=None, help="server address")
+    parser.add_argument("--port", default=None, type=int, help="server port")
 
     return parser.parse_args()
+
+def _get_env_var(name: str):
+    try:
+        return os.environ[name]
+
+    except KeyError as e:
+        raise RuntimeError(f"Missing required enviroment variable: {e}") from None
+
+def _get_host_port(args) -> tuple[str, int]:
+    host = args.host if args.host is not None else _get_env_var("APP_HOST")
+    port = args.port if args.port is not None else int(_get_env_var("APP_PORT"))
+    return (host, port)
 
 def main():
     """
     Entry point
     """
     args = _parse_args()
+    host, port = _get_host_port(args)
 
     database.init()
 
-    config = uvicorn.Config(APP_NAME, host=args.host, port=args.port)
+    config = uvicorn.Config(APP_NAME, host=host, port=port)
     server = uvicorn.Server(config)
     server.run()
 
